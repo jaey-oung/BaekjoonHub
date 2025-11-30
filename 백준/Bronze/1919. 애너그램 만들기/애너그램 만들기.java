33912201b136d568ad70443f81aa8ed55de6b3c6
Jaeyoung <64688247+jaey-oung@users.noreply.github.com>
2025-11-30 18:17:43 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 168 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+    public static int[] getAlphabetCount(String str) {
+        int[] count = new int[26]; // 알파벳 개수를 저장할 배열
+
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i) - 'a']++; // 해당 알파벳의 인덱스 위치에 개수 증가
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        int ans = 0;
+
+        int[] countA = getAlphabetCount(a); // 함수로 분리
+        int[] countB = getAlphabetCount(b);
+
+        for (int i = 0; i < 26; i++)
+            ans += Math.abs(countA[i] - countB[i]); // 절댓값으로 차이 계산
+        
+        System.out.println(ans);
+    }
+}
