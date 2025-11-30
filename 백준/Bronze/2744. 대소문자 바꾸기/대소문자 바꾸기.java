b1928e2d3885667359ad8ab6588260ade27396ee
Jaeyoung <64688247+jaey-oung@users.noreply.github.com>
2025-11-30 18:15:33 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 168 ms, Memory: 17516 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] ans = str.toCharArray(); // 문자열을 문자 배열로 변환
+        
+        for (int i = 0; i < str.length(); i++) {
+            if ('a' <= ans[i] && ans[i] <= 'z') // 소문자인 경우
+                ans[i] = (char)('A' + ans[i] - 'a'); // 대문자로 변환
+            else // 대문자인 경우
+                ans[i] = (char)('a' + ans[i] - 'A'); // 소문자로 변환
+        }
+        System.out.println(ans);
+    }
+}
